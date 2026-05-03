52767ec86eb8451888ae2a03cee2c8d1f0da3101
Nhat Tai NGUYEN <nhattai.nguyen88@gmail.com>
2026-05-03 21:34:56 +0200

    ➕ Use uv instead of poetry.

--- main.py
+++ main.py
@@ -132,7 +132,7 @@ def main():
     ]
     while count < 20:
         response = client.models.generate_content(
-            model="gemini-2.0-flash-001",
+            model="gemini-2.5-flash",
             contents=messages,
             config=types.GenerateContentConfig(
                 tools=[available_functions],
